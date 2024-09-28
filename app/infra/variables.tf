10c2aab643948a63107a764c49b74dbc549f4dbc
Naveenkumar Murugan <mndlsoft@gmail.com>
2024-09-28 22:54:59 +1000

    changes

--- app/infra/variables.tf
+++ app/infra/variables.tf
@@ -2,3 +2,14 @@ variable "account_id" {
   type        = string
   description = "The AWS account ID"
 }
+
+variable "key_name" {
+  description = "The SSH key pair to use for EC2 access"
+  type        = string
+}
+
+variable "postgres_password" {
+  description = "The password for the PostgreSQL 'postgres' user"
+  type        = string
+  sensitive   = true
+}
